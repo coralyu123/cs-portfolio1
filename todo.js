07816ff9078eb71ce88329c9b142a2052f2e4c18
coralyu123 <cyu.sh@mysummitps.org>
2016-11-08 23:28:26 +0000

    update

---
+++ todo.js
@@ -0,0 +1,22 @@
+var box1 = document.getElementById("box1");
+var button = document.getElementById("button");
+var inputButton;
+var div = document.getElementById("div");
+var font = document.getElementById("font");
+
+
+button.addEventListener("click", function(){
+    
+    var task = "<div>"+box1.value+"<div>" + "<br>";
+    div.innerHTML = div.innerHTML+task; 
+
+});
+
+div.addEventListener("click", function(evt){
+    
+    var thing = evt.target;
+    thing.parentNode.removeChild(thing);
+    
+});
+
+
