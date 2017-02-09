238a696398f444cd7ec47deddeb3d1042bfb861e
coralyu123 <cyu.sh@mysummitps.org>
2017-02-09 20:21:34 +0000

    update

--- todo.js
+++ todo.js
@@ -1,22 +1,20 @@
+var color = document.getElementById("color");
+var highlight = document.getElementById("highlight");
 var box1 = document.getElementById("box1");
 var button = document.getElementById("button");
-var inputButton;
-var div = document.getElementById("div");
-var color = document.getElementById("color");
+var list = document.getElementById("div");
 
-
-button.addEventListener("click", function(){
+button.addEventListener("click", function() {
     
-    var task = "<div>"+box1.value+"<div>" + "<br>";
-    div.innerHTML = div.innerHTML+task; 
+    
+    var task = "<div style = 'color: " + color.value + "; background-color:" + highlight.value + " '>" + box1.value + "<br>";
+    list.innerHTML = list.innerHTML + task;
     
 });
 
-div.addEventListener("click", function(evt){
-    
-    var thing = evt.target;
-    thing.parentNode.removeChild(thing);
-    
+list.addEventListener("click", function(evt){
+   
+   var remove = evt.target;
+   remove.parentNode.removeChild(remove);
+   
 });
-
-
